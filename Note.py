ef4b3c752a6ed8a7cc52f47468c23eb365138cc3
Evgeniia <jonnydor@gmail.com>
2024-04-13 21:29:32 +0300

    File functions added

--- Note.py
+++ Note.py
@@ -36,3 +36,9 @@ class Note:
 
     def set_date(note):
         note.date = str(datetime.now().strftime("%d.%m.%Y %H:%M:%S"))
+
+    def to_string(note):
+        return note.id + ';' + note.title + ';' + note.body + ';' + note.date
+
+    def map_note(note):
+        return '\nID: ' + note.id + '\n' + 'Заголовок: ' + note.title + '\n' + 'Текст: ' + note.body + '\n' + 'Дата создания: ' + note.date
